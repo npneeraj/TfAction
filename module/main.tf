4844a0fbc8d49453fe7822364f00241f5a8086f6
Neeraj <neeraj.panwar2016@gmail.com>
2023-03-17 16:07:54 +0530

    Initial Commit

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,7 @@
 module "ec2" {
     source = "../ec2"
-    ami-id = "ami-05afd67c4a44cc983"
-    instance-type = "t2.micro"
+
+    ami_id = "ami-05afd67c4a44cc983"
+    instance_type = "t2.micro"
   
 }

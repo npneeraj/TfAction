fb01cfe00f3d0a1c73363a0807ddf269d63dfaef
Neeraj <neeraj.panwar2016@gmail.com>
2023-03-17 16:13:46 +0530

    Initial Commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = var.ami_id
     instance_type = var.instance_type
+    subnet_id = "subnet-0962069167a59d185"
     tags = {
         Name = "Neeraj"
         Owner = "neeraj.panwar@cloudeq.com"

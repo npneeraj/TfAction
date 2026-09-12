6020e12f7c4eb49fd66d1af1037438752c061002
Neeraj Panwar <49247372+npneeraj@users.noreply.github.com>
2026-09-12 17:19:03 +0530

    Enhance variable.tf with descriptions

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,8 +1,20 @@
-variable "ami_id"{
-    type = string
+variable "ami_id" {
+  type        = string
+  description = "The AMI ID to use for the EC2 instance"
 }
 
 variable "instance_type" {
-    type = string
-    
+  type        = string
+  description = "The type of EC2 instance to launch"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The VPC Subnet ID to launch the instance in"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags to assign to the resource"
+  default     = {}
 }

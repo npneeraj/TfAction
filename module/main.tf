c4fd939e6e2e1197f833e502ae86bb3d38622e69
Neeraj Panwar <49247372+npneeraj@users.noreply.github.com>
2026-09-12 18:29:31 +0530

    optimise for dynamic use-case

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,30 @@
-module "ec2" {
-    source = "../ec2"
+# 1. Dynamically fetch the latest Amazon Linux 2023 AMI
+data "aws_ssm_parameter" "amazon_linux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+}
 
-    ami_id = "ami-05afd67c4a44cc983"
-    instance_type = "t2.micro"
+# 2. Fetch the Default VPC for the region the pipeline is running in
+data "aws_vpc" "default" {
+  default = true
+}
+
+# 3. Fetch all available Subnets within that Default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+module "ec2" {
+  source = "../ec2"
+  ami_id        = data.aws_ssm_parameter.amazon_linux_2023.value
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnets.default.ids[0]
   
+  tags = {
+    Name    = "Neeraj"
+    Owner   = "neeraj.panwar@cloudeq.com"
+    Purpose = "training"
+  }
 }

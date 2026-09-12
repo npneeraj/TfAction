9c7541b727986f8c0c704e1078208e7a00c2ae14
Neeraj Panwar <49247372+npneeraj@users.noreply.github.com>
2026-09-12 17:36:18 +0530

    refactor instance resource configuration

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,7 @@
 resource "aws_instance" "ec2" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    subnet_id = "subnet-0962069167a59d185"
-    tags = {
-        Name = "Neeraj"
-        Owner = "neeraj.panwar@cloudeq.com"
-        Purpose = "training"
-    }
-    volume_tags = {
-        Name = "Neeraj"
-        Owner = "neeraj.panwar@cloudeq.com"
-        Purpose = "training"
-      
-    }
-  
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  tags          = var.tags
+  volume_tags   = var.tags
 }
